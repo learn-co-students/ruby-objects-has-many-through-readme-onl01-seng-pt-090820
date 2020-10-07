a2d4b9230fdc2e01bf2050f4be3bd9f4e7a7eeb5
Carl J <carlj1881@yahoo.com>
2020-10-07 06:50:24 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,44 @@
+require "pry"
+
+
 class Waiter
 
+  attr_accessor :name, :experience
+
+  @@all = []
+
+  def initialize(name, experience)
+    @name = name
+    @experience = experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip)
+   Meal.new(self, customer, total, tip)
+  end
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
+  end
+  
+  def best_tipper
+    biggest_tip = 0
+    biggest_tipper = "Uncle Money Bags"
+    
+    Meal.all.select do |meal|
+      # binding.pry
+      if biggest_tip < meal.tip
+        biggest_tip = meal.tip
+        biggest_tipper = meal.customer
+      end
+    end
+    return biggest_tipper
+  end
+  
 end
